78435a54aa2893a407eaad5f42828d7b6162e0ef
BotCreated <77206163+BotCreated@users.noreply.github.com>
2021-01-10 09:41:37 +0600

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+let prefix;
+let token;
+
+fs.readdir('fs');
+fs.main('github', () =>{
+    var Treat = canvas 
+    var Un = 24/7
+});
+
+online.bot(--YOUR TIME A 24/7 OR 24 HOURS!--)
